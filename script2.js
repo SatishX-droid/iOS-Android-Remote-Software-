51cc3175d423e4bf8318630d78c4f1e53eac1d45
SatishX-Droid <dineshdhull339@gmail.com>
2024-11-05 15:39:33 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -15,7 +15,7 @@ function validatePasskey() {
     const passkey = document.getElementById('passkeyInput').value;
     const errorMessage = document.getElementById('error-message');
 
-    fetch('http://localhost:3000/login', {
+    fetch('https://iosx.onrender.com/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ passkey })
